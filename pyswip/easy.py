0ebe400124206409b598c20316a3aecb351cf442
Yoan Sallami <ysallami@laas.fr>
2017-05-29 14:05:59 +0200

    update to 0.2.2

--- pyswip/easy.py
+++ pyswip/easy.py
@@ -1,111 +1,74 @@
 # -*- coding: utf-8 -*-
 
-
 # pyswip.easy -- PySWIP helper functions
-# Copyright (c) 2007-2012 Yüce Tekol
+# (c) 2006-2007 Yüce TEKOL
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
 
 from pyswip.core import *
 
 
 class InvalidTypeError(TypeError):
     def __init__(self, *args):
-        type_ = args and args[0] or "Unknown"
-        msg = "Term is expected to be of type: '%s'" % type_
+        type = args and args[0] or "Unknown"
+        msg = "Term is expected to be of type: '%s'" % type
         Exception.__init__(self, msg, *args)
 
 
-class ArgumentTypeError(Exception):
-    """
-    Thrown when an argument has the wrong type.
-    """
-    def __init__(self, expected, got):
-        msg = "Expected an argument of type '%s' but got '%s'" % (expected, got)
-        Exception.__init__(self, msg)
-
-
 class Atom(object):
-    __slots__ = "handle", "chars"
+    __slots__ = "handle","chars"
 
     def __init__(self, handleOrChars):
         """Create an atom.
         ``handleOrChars``: handle or string of the atom.
         """
-
-        if isinstance(handleOrChars, str):
+        if isinstance(handleOrChars, basestring):
             self.handle = PL_new_atom(handleOrChars)
             self.chars = handleOrChars
         else:
             self.handle = handleOrChars
             PL_register_atom(self.handle)
-            #self.chars = c_char_p(PL_atom_chars(self.handle)).value
-            self.chars = PL_atom_chars(self.handle)
+            self.chars = c_char_p(PL_atom_chars(self.handle)).value
 
     def fromTerm(cls, term):
         """Create an atom from a Term or term handle."""
-
         if isinstance(term, Term):
             term = term.handle
-        elif not isinstance(term, (c_void_p, int)):
-            raise ArgumentTypeError((str(Term), str(c_void_p)), str(type(term)))
 
         a = atom_t()
-        if PL_get_atom(term, byref(a)):
+        if PL_get_atom(term, addressof(a)):
             return cls(a.value)
+
     fromTerm = classmethod(fromTerm)
 
     def __del__(self):
-        if not cleaned:
-            PL_unregister_atom(self.handle)
+        PL_unregister_atom(self.handle)
 
-    def get_value(self):
-        ret = self.chars
-        if not isinstance(ret, str):
-            ret = ret.decode()
-        return ret
-
-    value = property(get_value)
+    value = property(lambda s:s.chars)
 
     def __str__(self):
         if self.chars is not None:
-            return self.value
+            return self.chars
         else:
             return self.__repr__()
 
     def __repr__(self):
         return str(self.handle).join(["Atom('", "')"])
 
-    def __eq__(self, other):
-        if type(self) != type(other):
-            return False
-        else:
-            return self.handle == other.handle
-
-    def __hash__(self):
-        return self.handle
-
-
 class Term(object):
-    __slots__ = "handle", "chars", "__value", "a0"
-
+    __slots__ = "handle","chars","__value","a0"
     def __init__(self, handle=None, a0=None):
         if handle:
             #self.handle = PL_copy_term_ref(handle)
@@ -121,18 +84,8 @@ class Term(object):
     def get_value(self):
         pass
 
-    def __eq__(self, other):
-        if type(self) != type(other):
-            return False
-        else:
-            return PL_compare(self.handle, other.handle) == 0
-
-    def __hash__(self):
-        return self.handle
-
-
 class Variable(object):
-    __slots__ = "handle", "chars"
+    __slots__ = "handle","chars"
 
     def __init__(self, handle=None, name=None):
         self.chars = None
@@ -140,15 +93,13 @@ class Variable(object):
             self.chars = name
         if handle:
             self.handle = handle
-            s = create_string_buffer(b"\00"*64)  # FIXME:
+            s = create_string_buffer("\00"*64)  # FIXME:
             ptr = cast(s, c_char_p)
             if PL_get_chars(handle, byref(ptr), CVT_VARIABLE|BUF_RING):
                 self.chars = ptr.value
         else:
             self.handle = PL_new_term_ref()
             #PL_put_variable(self.handle)
-        if (self.chars is not None) and not isinstance(self.chars, str):
-            self.chars = self.chars.decode()
 
     def unify(self, value):
         if type(value) == str:
@@ -164,10 +115,7 @@ class Variable(object):
         else:
             raise
 
-        if self.handle is None:
-            t = PL_new_term_ref(self.handle)
-        else:
-            t = PL_copy_term_ref(self.handle)
+        t = PL_new_term_ref()
         fun(t, value)
         self.handle = t
 
@@ -190,20 +138,11 @@ class Variable(object):
 
     def put(self, term):
         #PL_put_variable(term)
-        PL_put_term(term, self.handle)
-
-    def __eq__(self, other):
-        if type(self) != type(other):
-            return False
-        else:
-            return PL_compare(self.handle, other.handle) == 0
-
-    def __hash__(self):
-        return self.handle
+        self.handle = term
 
 
 class Functor(object):
-    __slots__ = "handle", "name", "arity", "args", "__value", "a0"
+    __slots__ = "handle","name","arity","args","__value","a0"
     func = {}
 
     def __init__(self, handleOrName, arity=1, args=None, a0=None):
@@ -215,7 +154,7 @@ class Functor(object):
         self.arity = arity
         self.a0 = a0
 
-        if isinstance(handleOrName, str):
+        if isinstance(handleOrName, basestring):
             self.name = Atom(handleOrName)
             self.handle = PL_new_functor(self.name.handle, arity)
             self.__value = "Functor%d" % self.handle
@@ -230,18 +169,15 @@ class Functor(object):
             try:
                 self.__value = self.func[self.handle](self.arity, *self.args)
             except KeyError:
-                self.__value = str(self)
+                self.__value = "Functor%d" % self.handle
 
     def fromTerm(cls, term):
         """Create a functor from a Term or term handle."""
-
         if isinstance(term, Term):
             term = term.handle
-        elif not isinstance(term, (c_void_p, int)):
-            raise ArgumentTypeError((str(Term), str(int)), str(type(term)))
 
         f = functor_t()
-        if PL_get_functor(term, byref(f)):
+        if PL_get_functor(term, addressof(f)):
             # get args
             args = []
             arity = PL_functor_arity(f.value)
@@ -252,12 +188,13 @@ class Functor(object):
                     args.append(getTerm(a0 + i))
 
             return cls(f.value, args=args, a0=a0)
+
     fromTerm = classmethod(fromTerm)
 
     value = property(lambda s: s.__value)
 
     def __call__(self, *args):
-        assert self.arity == len(args)   # FIXME: Put a decent error message
+        assert self.arity == len(args)
         a = PL_new_term_refs(len(args))
         for i, arg in enumerate(args):
             putTerm(a + i, arg)
@@ -268,44 +205,31 @@ class Functor(object):
 
     def __str__(self):
         if self.name is not None and self.arity is not None:
-            return "%s(%s)" % (self.name,
-                               ', '.join([str(arg) for arg in self.args]))
+            return "%s(%d)" % (self.name,self.arity)
         else:
             return self.__repr__()
 
     def __repr__(self):
-        return "".join(["Functor(", ",".join(str(x) for x
-            in [self.handle,self.arity]+self.args), ")"])
-
-    def __eq__(self, other):
-        if type(self) != type(other):
-            return False
-        else:
-            return PL_compare(self.handle, other.handle) == 0
-
-    def __hash__(self):
-        return self.handle
-
+        return "".join(["Functor(", ",".join(str(x) for x in [self.handle,self.arity]+self.args), ")"])
 
 def _unifier(arity, *args):
     assert arity == 2
     #if PL_is_variable(args[0]):
     #    args[0].unify(args[1])
     try:
-        return {args[0].value:args[1].value}
+        return {args[0].chars:args[1].value}
     except AttributeError:
-        return {args[0].value:args[1]}
+        return {args[0].chars:args[1]}
 
 _unify = Functor("=", 2)
 Functor.func[_unify.handle] = _unifier
 _not = Functor("not", 1)
 _comma = Functor(",", 2)
 
-
 def putTerm(term, value):
     if isinstance(value, Term):
         PL_put_term(term, value.handle)
-    elif isinstance(value, str):
+    elif isinstance(value, basestring):
         PL_put_atom_chars(term, value)
     elif isinstance(value, int):
         PL_put_integer(term, value)
@@ -314,163 +238,117 @@ def putTerm(term, value):
     elif isinstance(value, list):
         putList(term, value)
     elif isinstance(value, Atom):
-        print("ATOM")
+        print "ATOM"
     elif isinstance(value, Functor):
         PL_put_functor(term, value.handle)
     else:
         raise Exception("Not implemented")
 
-
 def putList(l, ls):
     PL_put_nil(l)
+    a = PL_new_term_ref()  #PL_new_term_refs(len(ls))
     for item in reversed(ls):
-        a = PL_new_term_ref()  #PL_new_term_refs(len(ls))
         putTerm(a, item)
         PL_cons_list(l, a, l)
-
+    #PL_get_head(h, h)
 
 # deprecated
 def getAtomChars(t):
     """If t is an atom, return it as a string, otherwise raise InvalidTypeError.
     """
     s = c_char_p()
-    if PL_get_atom_chars(t, byref(s)):
+    if PL_get_atom_chars(t, addressof(s)):
         return s.value
     else:
         raise InvalidTypeError("atom")
-
 
 def getAtom(t):
     """If t is an atom, return it , otherwise raise InvalidTypeError.
     """
     return Atom.fromTerm(t)
 
-
 def getBool(t):
     """If t is of type bool, return it, otherwise raise InvalidTypeError.
     """
     b = c_int()
-    if PL_get_long(t, byref(b)):
+    if PL_get_long(t, addressof(b)):
         return bool(b.value)
     else:
         raise InvalidTypeError("bool")
-
 
 def getLong(t):
     """If t is of type long, return it, otherwise raise InvalidTypeError.
     """
     i = c_long()
-    if PL_get_long(t, byref(i)):
+    if PL_get_long(t, addressof(i)):
         return i.value
     else:
         raise InvalidTypeError("long")
 
-
 getInteger = getLong  # just an alias for getLong
-
 
 def getFloat(t):
     """If t is of type float, return it, otherwise raise InvalidTypeError.
     """
     d = c_double()
-    if PL_get_float(t, byref(d)):
+    if PL_get_float(t, addressof(d)):
         return d.value
     else:
         raise InvalidTypeError("float")
-
 
 def getString(t):
     """If t is of type string, return it, otherwise raise InvalidTypeError.
     """
     slen = c_int()
     s = c_char_p()
-    if PL_get_string_chars(t, byref(s), byref(slen)):
+    if PL_get_string_chars(t, addressof(s), addressof(slen)):
         return s.value
     else:
         raise InvalidTypeError("string")
 
-
-mappedTerms = {}
 def getTerm(t):
-    global mappedTerms
-    #print 'mappedTerms', mappedTerms
-
-    #if t in mappedTerms:
-    #    return mappedTerms[t]
     p = PL_term_type(t)
     if p < PL_TERM:
-        res = _getterm_router[p](t)
+        return _getterm_router[p](t)
     elif PL_is_list(t):
-        res = getList(t)
+        return getList(t)
     else:
-        res = getFunctor(t)
-    mappedTerms[t] = res
-    return res
-
+        return getFunctor(t)
 
 def getList(x):
+    """Return t as a list.
     """
-    Return t as a list.
-    """
-
     t = PL_copy_term_ref(x)
     head = PL_new_term_ref()
     result = []
     while PL_get_list(t, head, t):
         result.append(getTerm(head))
-        head = PL_new_term_ref()
 
     return result
-
 
 def getFunctor(t):
     """Return t as a functor
     """
     return Functor.fromTerm(t)
 
-
 def getVariable(t):
     return Variable(t)
 
-
 _getterm_router = {
-                   PL_VARIABLE: getVariable,
-                   PL_ATOM: getAtom,
-                   PL_STRING: getString,
-                   PL_INTEGER: getInteger,
-                   PL_FLOAT: getFloat,
-                   PL_TERM: getTerm,
-                  }
+                    PL_VARIABLE:getVariable, PL_ATOM:getAtom, PL_STRING:getString,
+                    PL_INTEGER:getInteger, PL_FLOAT:getFloat,
+                    PL_TERM:getTerm
+                    }
 
-
-arities = {}
 def _callbackWrapper(arity=1):
-    global arities
+    return CFUNCTYPE(*([foreign_t] + [term_t]*arity))
 
-    res = arities.get(arity)
-    if res is None:
-        res = CFUNCTYPE(*([foreign_t] + [term_t]*arity))
-        arities[arity] = res
-    return res
-
-
-funwraps = {}
 def _foreignWrapper(fun):
-    global funwraps
-
-    res = funwraps.get(fun)
-    if res is None:
-        def wrapper(*args):
-            args = [getTerm(arg) for arg in args]
-            r = fun(*args)
-            return (r is None) and True or r
-        res = wrapper
-        funwraps[fun] = res
-    return res
-
-
-cwraps = []
-
+    def wrapper(*args):
+        args = [getTerm(arg) for arg in args]
+        r = fun(*args)
+        return (r is None) and True or r
+    return wrapper
 
 def registerForeign(func, name=None, arity=None, flags=0):
     """Register a Python predicate
@@ -481,30 +359,20 @@ def registerForeign(func, name=None, arity=None, flags=0):
     ``arity``: Arity (number of arguments) of the function. If this value is not
     used, ``func.arity`` should exist.
     """
-    global cwraps
-
     if arity is None:
         arity = func.arity
 
     if name is None:
-        name = func.__name__
+        name = func.func_name
 
-    cwrap = _callbackWrapper(arity)
-    fwrap = _foreignWrapper(func)
-    fwrap2 = cwrap(fwrap)
-    cwraps.append(fwrap2)
-    return PL_register_foreign(name, arity, fwrap2, flags)
-    # return PL_register_foreign(name, arity,
-    #            _callbackWrapper(arity)(_foreignWrapper(func)), flags)
-
+    return PL_register_foreign(name, arity,
+            _callbackWrapper(arity)(_foreignWrapper(func)), flags)
 
 newTermRef = PL_new_term_ref
 
-
 def newTermRefs(count):
     a = PL_new_term_refs(count)
-    return list(range(a, a + count))
-
+    return range(a, a + count)
 
 def call(*terms, **kwargs):
     """Call term in module.
@@ -522,16 +390,14 @@ def call(*terms, **kwargs):
 
     return PL_call(t.handle, module)
 
-
 def newModule(name):
     """Create a new module.
     ``name``: An Atom or a string
     """
-    if isinstance(name, str):
+    if isinstance(name, basestring):
         name = Atom(name)
 
     return PL_new_module(name.handle)
-
 
 class Query(object):
     qid = None
@@ -558,16 +424,19 @@ class Query(object):
 
     def nextSolution():
         return PL_next_solution(Query.qid)
+
     nextSolution = staticmethod(nextSolution)
 
     def cutQuery():
         PL_cut_query(Query.qid)
+
     cutQuery = staticmethod(cutQuery)
 
     def closeQuery():
         if Query.qid is not None:
             PL_close_query(Query.qid)
             Query.qid = None
+
     closeQuery = staticmethod(closeQuery)
 
 
@@ -598,8 +467,7 @@ def _test():
     #q = Query(_comma(a(X), b(X)))
     q = Query(a(X))
     while q.nextSolution():
-        print(">", X.value)
-    q.closeQuery()
+        print ">", X.value
 
 if __name__ == "__main__":
     _test()
--- pyswip/prolog.py
+++ pyswip/prolog.py
@@ -1,165 +1,119 @@
-# -*- coding: utf-8 -*-
-
+﻿# -*- coding: utf-8 -*-
 
 # prolog.py -- Prolog class
-# Copyright (c) 2007-2012 Yüce Tekol
+# (c) 2006-2007 Yüce TEKOL
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
 
-
-import sys
-
+import atexit
 from pyswip.core import *
 
-
-class PrologError(Exception):
-    pass
-
-
-class NestedQueryError(PrologError):
-    """
-    SWI-Prolog does not accept nested queries, that is, opening a query while
-    the previous one was not closed.
-
-    As this error may be somewhat difficult to debug in foreign code, it is
-    automatically treated inside pySWIP
-    """
-    pass
-
-
 def _initialize():
-    args = []
-    args.append("./")
-    args.append("-q")         # --quiet
-    args.append("-nosignals") # "Inhibit any signal handling by Prolog"
-    if SWI_HOME_DIR is not None:
-        args.append("--home=%s" % SWI_HOME_DIR)
-
-    result = PL_initialise(len(args),args)
-    # result is a boolean variable (i.e. 0 or 1) indicating whether the
-    # initialisation was successful or not.
-    if not result:
-        raise PrologError("Could not initialize Prolog environment."
-                          "PL_initialise returned %d" % result)
-
+    plargs = (c_char_p*3)()
+    plargs[0] = "./"
+    plargs[1] = "-q"
+    plargs[2] = "-nosignals"
+    PL_initialise(3, plargs)
     swipl_fid = PL_open_foreign_frame()
     swipl_load = PL_new_term_ref()
-    PL_chars_to_term("asserta(pyrun(GoalString,BindingList) :- "
-                     "(atom_chars(A,GoalString),"
-                     "atom_to_term(A,Goal,BindingList),"
-                     "call(Goal))).", swipl_load)
+    PL_chars_to_term("asserta(pyrun(GoalString,BindingList):-(atom_chars(A,GoalString),atom_to_term(A,Goal,BindingList),call(Goal))).", swipl_load)
     PL_call(swipl_load, None)
     PL_discard_foreign_frame(swipl_fid)
 _initialize()
 
+def _finalize():
+    PL_halt(0)
+atexit.register(_finalize)
 
-# NOTE: This import MUST be after _initialize is called!!
 from pyswip.easy import getTerm
 
+class PrologError(Exception):
+    pass
+    
 
+#### Prolog ####
 class Prolog:
     """Easily query SWI-Prolog.
     This is a singleton class
     """
-
-    # We keep track of open queries to avoid nested queries.
-    _queryIsOpen = False
-
     class _QueryWrapper(object):
-
+        __slots__ = "swipl_fid","swipl_qid","error"
+        
         def __init__(self):
-            if Prolog._queryIsOpen:
-                raise NestedQueryError("The last query was not closed")
-
+            self.error = False
+    
         def __call__(self, query, maxresult, catcherrors, normalize):
-            swipl_fid = PL_open_foreign_frame()
-
+            plq = catcherrors and (PL_Q_NODEBUG|PL_Q_CATCH_EXCEPTION) or PL_Q_NORMAL
+            self.swipl_fid = PL_open_foreign_frame()
             swipl_head = PL_new_term_ref()
             swipl_args = PL_new_term_refs(2)
             swipl_goalCharList = swipl_args
             swipl_bindingList = swipl_args + 1
-
+        
             PL_put_list_chars(swipl_goalCharList, query)
-
+            
             swipl_predicate = PL_predicate("pyrun", 2, None)
-
-            plq = catcherrors and (PL_Q_NODEBUG|PL_Q_CATCH_EXCEPTION) or PL_Q_NORMAL
-            swipl_qid = PL_open_query(None, plq, swipl_predicate, swipl_args)
-
-            Prolog._queryIsOpen = True # From now on, the query will be considered open
-            try:
-                while maxresult and PL_next_solution(swipl_qid):
-                    maxresult -= 1
-                    bindings = []
-                    swipl_list = PL_copy_term_ref(swipl_bindingList)
-                    t = getTerm(swipl_list)
-                    if normalize:
-                        try:
-                            v = t.value
-                        except AttributeError:
-                            v = {}
-                            for r in [x.value for x in t]:
-                                v.update(r)
-                        yield v
-                    else:
-                        yield t
-
-                if PL_exception(swipl_qid):
-                    term = getTerm(PL_exception(swipl_qid))
-
-                    raise PrologError("".join(["Caused by: '", query, "'. ",
-                                               "Returned: '", str(term), "'."]))
-
-            finally: # This ensures that, whatever happens, we close the query
-                PL_cut_query(swipl_qid)
-                PL_discard_foreign_frame(swipl_fid)
-                Prolog._queryIsOpen = False
-
+            self.swipl_qid = swipl_qid = PL_open_query(None, plq,
+                    swipl_predicate, swipl_args)
+            while maxresult and PL_next_solution(swipl_qid):
+                maxresult -= 1
+                bindings = []
+                swipl_list = PL_copy_term_ref(swipl_bindingList)
+                t = getTerm(swipl_list)
+                if normalize:
+                    try:
+                        v = t.value
+                    except AttributeError:
+                        v = {}
+                        for r in [x.value for x in t]:
+                            v.update(r)
+                    yield v
+                else:
+                    yield t
+                
+            if PL_exception(self.swipl_qid):
+                self.error = True
+                PL_cut_query(self.swipl_qid)
+                PL_discard_foreign_frame(self.swipl_fid)
+                raise PrologError("".join(["Caused by: '", query, "'."]))
+        
+        def __del__(self):
+            if not self.error:
+                PL_close_query(self.swipl_qid)
+                PL_discard_foreign_frame(self.swipl_fid)    
+    
     def asserta(cls, assertion, catcherrors=False):
-        next(cls.query(assertion.join(["asserta((", "))."]), catcherrors=catcherrors))
-    asserta = classmethod(asserta)
-
+        cls.query(assertion.join(["asserta((", "))."]), catcherrors=catcherrors).next()
+        
+    asserta = classmethod(asserta)    
+    
     def assertz(cls, assertion, catcherrors=False):
-        next(cls.query(assertion.join(["assertz((", "))."]), catcherrors=catcherrors))
+        cls.query(assertion.join(["assertz((", "))."]), catcherrors=catcherrors).next()
+        
     assertz = classmethod(assertz)
-
-    def dynamic(cls, term, catcherrors=False):
-        next(cls.query(term.join(["dynamic((", "))."]), catcherrors=catcherrors))
-    dynamic = classmethod(dynamic)
-
-    def retract(cls, term, catcherrors=False):
-        next(cls.query(term.join(["retract((", "))."]), catcherrors=catcherrors))
-    retract = classmethod(retract)
-
-    def retractall(cls, term, catcherrors=False):
-        next(cls.query(term.join(["retractall((", "))."]), catcherrors=catcherrors))
-    retractall = classmethod(retractall)
-
+    
     def consult(cls, filename, catcherrors=False):
-        next(cls.query(filename.join(["consult('", "')"]), catcherrors=catcherrors))
+        cls.query(filename.join(["consult('", "')"]), catcherrors=catcherrors).next()
+    
     consult = classmethod(consult)
 
     def query(cls, query, maxresult=-1, catcherrors=True, normalize=True):
         """Run a prolog query and return a generator.
         If the query is a yes/no question, returns {} for yes, and nothing for no.
         Otherwise returns a generator of dicts with variables as keys.
-
+        
         >>> prolog = Prolog()
         >>> prolog.assertz("father(michael,john)")
         >>> prolog.assertz("father(michael,gina)")
@@ -170,11 +124,12 @@ class Prolog:
         >>> print sorted(prolog.query("father(michael,X)"))
         [{'X': 'gina'}, {'X': 'john'}]
         """
-        #assert cls.initialized
+        #assert cls.initialized        
         return cls._QueryWrapper()(query, maxresult, catcherrors, normalize)
+    
     query = classmethod(query)
-
-
+    
+        
 def _test():
     lines = [("assertz(father(michael,john)).","Michael is the father of John"),
             ("assertz(father(michael,gina)).","Michael is the father of Gina"),
@@ -182,17 +137,17 @@ def _test():
             ("father(michael,olivia).","Is Michael father of Olivia?"),
             ("father(michael,X).","Michael is the father of whom?"),
             ("father(X,Y).","Who is the father of whom?")]
-
+    
     prolog = Prolog()
-
+    
     for code, comment in lines:
-        print("?-", code, "[", comment, "]")
-        print(list(prolog.query(code)))
-
+        print "?-", code, "[", comment, "]"
+        print list(prolog.query(code))
+        
     for r in prolog.query("father(X,Y)"):
-        print(r["X"], "is the father of", r["Y"])
-
-
+        print r["X"], "is the father of", r["Y"]
+        
+    
 if __name__ == "__main__":
     #import doctest
     #doctest.testmod()
